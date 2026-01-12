6421038463624ce64e07db45e0e905438f2b418b
Shabarinath <121168565+Shabarinathr@users.noreply.github.com>
2026-01-12 06:02:52 +0000

    Day 2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # python-dsa-journey
+
+## Day 2 Progress ✅
+- ✅ Variables (int, float, str, bool)
+- ✅ Basic calculations 
+- ✅ type() function
+- ✅ String concatenation with str()
---
+++ week1/day02_variables.py
@@ -0,0 +1,75 @@
+# # Day 2 - Variables and data types
+
+# #integer
+# age=21
+# year=2026
+
+# #Float
+# height=5.9
+# temperature=36.5
+
+# #String 
+# name="Shabarinath"
+# College="Pillai College"
+
+# #Boolean
+# is_student=True
+# has_internship=True
+
+# #Print Variables
+# print(name)
+# print("Age: ",age)
+# print("Height: ",height)
+# print("is_student:",is_student)
+
+
+# Excercise 1
+
+# first_name="Shabarinath"
+# last_name="Raghunath"
+# age=21
+# city="Mumbai"
+
+# print(first_name + " " + last_name + " is " + str(age) + " years old")
+
+
+# Exercise 2
+
+# num1=10
+# num2=5
+
+# addition = num1 + num2
+# substraction = num1 - num2
+# division = num1 / num2
+# multiplication = num1 * num2
+# print(division)
+
+
+# Exercise 3 
+
+# x = 2
+# y =3.14
+# z = "Hello"
+# w = True
+
+# print(type(w))
+
+
+# Exercise 4
+
+# l = 14
+# b = 3
+# area = l * b
+# print("area =",area)
+
+# radius = 7
+# pi = 3.14
+# circle_area = pi * radius * radius
+# print("Area: ",circle_area)
+
+
+# Additional
+
+# company="Unifeeder"
+# project="AIS"
+# print(f"Working at {company} on {project}")
