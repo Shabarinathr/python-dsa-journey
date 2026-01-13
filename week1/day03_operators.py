df48a16157004a00de62250a52f9a24a9e433b7d
Shabarinath <121168565+Shabarinathr@users.noreply.github.com>
2026-01-13 04:40:02 +0000

    Day 3

---
+++ week1/day03_operators.py
@@ -0,0 +1,51 @@
+# #  day 3 Comparision operators
+
+# age = 21
+# internship_months = 3 
+
+# print("age==21:",age==21)
+# print("age !=20:",age!=20)
+# print("age>18:",age>18)
+# print("age<25:",age<25)
+# print("age>=21:",age=21)
+
+# Excercise 1
+
+# age=21
+# voting_age=18
+
+# can_vote = age >= voting_age
+# print("Can vote:",can_vote)
+
+# Excercise 2
+
+# marks = 70
+# pass_marks = 40
+
+# print("Passed",marks>=pass_marks)
+# print("Distinction",marks>=75)
+
+# Combine Multiple Operators
+# is_student=True
+# has_internship=True
+# age_above_18=True
+
+# AND 
+# print("Full time student:",is_student and has_internship and age_above_18)
+
+# OR 
+# print("Either student or intern:",is_student or has_internship)
+
+#NOT
+# print("Not a beginner:",not True)
+
+
+# Challenge
+# tcs eligiblity test
+
+# gpa = 7.0
+# tenth = 79
+# predegree = 59
+
+# eligible = (gpa>=6.0) and (tenth >= 60) and (predegree >= 60)
+# print("TCS Ninja Eligiblity:", eligible)
